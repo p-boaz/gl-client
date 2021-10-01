fa91be468c01bb5084da12a6490cbf2d0b4078d5
Peter Boaz <62269968+p-boaz@users.noreply.github.com>
2021-09-30 23:26:32 -0700

    Button goes to DA linked session

--- src/components/ResumeButton.vue
+++ src/components/ResumeButton.vue
@@ -1,82 +1,37 @@
 <template>
   <div>
-    <button
-      class="btn m-3 text-sm mt-6"
-    >
-      {{ data }}
-    </button>
+    <a rel="noreferrer" href="{{ sessionUrl }}" target="_blank" class="bg-red-500 hover:bg-red-400 border-b-4 border-red-700 hover:border-red-500 text-white text-center py-2 px-4 rounded">
+      Add more data
+    </a>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, computed } from 'vue'
 const api = import.meta.env.VITE_DA_API_KEY
 
-export default defineComponent({
+export default {
   name: 'ResumeButton',
   props: {
-    id: String,
+    id: null,
   },
-  setup(props) {
-    const data = ref(null)
-    const loading = ref(true)
-    const error = ref(null)
-
-    function fetchData() {
-      loading.value = true
-      // I prefer to use fetch
-      // you can use use axios as an alternative
-      return fetch(`https://ny.barplaybook.com/api/resume_url?key=${api}`, {
-        method: 'post',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify ({
-          session: `${props.id}`,
-          i: 'docassemble.testServer:data/questions/aaDataTypes.yml',
-        }),
-      })
-        .then((res) => { console.log(res) })
-        .catch((res) => { console.log(res) })
-        .then((res) => {
-          // a non-200 response code
-          if (!res.ok) {
-            // create error instance with HTTP status text
-            const error = new Error(res.statusText)
-            error.json = res.json()
-            throw error
-          }
-
-          return res.json()
-        })
-        .then((json) => {
-          // set the response data
-          data.value = json.data
-        })
-        .catch((err) => {
-          error.value = err
-          // In case a custom JSON error response was provided
-          if (err.json) {
-            return err.json.then((json) => {
-              // set the JSON response message
-              error.value.message = json.message
-            })
-          }
-        })
-        .then(() => {
-          loading.value = false
-        })
-    }
-
-    onMounted(() => {
-      fetchData()
-    })
-
+  data() {
     return {
-      data,
-      loading,
-      error,
+      sessionUrl: null,
     }
   },
-})
+  async created() {
+    // POST request using fetch with async/await
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify ({
+        session: this.id,
+        i: 'docassemble.testServer:data/questions/aaDataTypes.yml',
+      }),
+    }
+    const response = await fetch(`https://ny.barplaybook.com/api/resume_url?key=${api}`, requestOptions)
+    const data = await response.json()
+    this.sessionUrl = data
+  },
+}
 </script>
